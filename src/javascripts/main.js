c3da0aada38ce3ad4e6532c4da126fa85e8a0d4b
szaizen <zaizen.saki@gmail.com>
2019-09-27 11:29:35 +0900

    条件分岐とループ

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -14,3 +14,21 @@ var app2 = new Vue({
     message: 'You loaded this page on ' + new Date().toLocaleString()
   }
 })
+
+// 条件分岐とループ
+var app3 = new Vue({
+  el: '#app-3',
+  data: {
+    seen: true
+  }
+})
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    todos: [
+      { text: 'Learn JavaScript' },
+      { text: 'Learn Vue' },
+      { text: 'Build something awesome' }
+    ]
+  }
+})

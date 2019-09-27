98d49a940324302cf7c1fbfd8a0fc7ae12b802fe
szaizen <zaizen.saki@gmail.com>
2019-09-27 11:25:22 +0900

    要素の属性を束縛（バインディング）

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,6 +1,16 @@
+
+// 宣言的レンタリング
 var app = new Vue({
   el: '#app',
   data: {
     message: 'Hello Vue!'
+  }
+})
+
+// 要素の属性を束縛（バインディング）
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: 'You loaded this page on ' + new Date().toLocaleString()
   }
 })

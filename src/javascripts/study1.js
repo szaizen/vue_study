8c5eaa9a006eb886f7433a8b21923bcd4438a72a
szaizen <zaizen.saki@gmail.com>
2019-10-03 17:28:32 +0900

    データの登録と更新

---
+++ src/javascripts/study1.js
@@ -0,0 +1,11 @@
+new Vue({
+  el: '#app1',
+  data: {
+    name: 'test',
+    list: [
+      {id: 1, name: 'スライム', hp: 100},
+      {id: 2, name: 'ゴブリン', hp: 200},
+      {id: 3, name: 'ドラゴン', hp: 500}
+    ]
+  }
+})
